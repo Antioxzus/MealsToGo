44cbff47911afa51986a94e8e55f0def329c54b4
Antioxzus <annamalinn@hotmail.com>
2022-08-07 21:26:18 +0200

    preparing for functioning searchbar

--- assets/open.js
+++ assets/open.js
@@ -2,9 +2,9 @@ export default `<?xml version="1.0" encoding="iso-8859-1"?>
 <!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0)  -->
 <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="0 0 511.952 511.952" style="enable-background:new 0 0 511.952 511.952;" xml:space="preserve">
-<path style="fill:#F2F2F2;" d="M410.643,161.145L43.709,251.811l137.6-230.4L410.643,161.145z M74.643,228.345l297.6-73.6
+<path style="fill:#cdb4db;" d="M410.643,161.145L43.709,251.811l137.6-230.4L410.643,161.145z M74.643,228.345l297.6-73.6
 	l-185.6-112L74.643,228.345z"/>
-<path style="fill:#FF7058;" d="M510.909,373.411l-52.267-212.267c-4.267-17.067-22.4-27.733-39.467-23.467L24.509,235.811
+<path style="fill:#cdb4db;" d="M510.909,373.411l-52.267-212.267c-4.267-17.067-22.4-27.733-39.467-23.467L24.509,235.811
 	c-17.067,4.267-27.733,21.333-23.467,38.4l52.267,213.333c4.267,17.067,22.4,27.733,39.467,23.467l394.667-98.133
 	C504.509,408.611,515.176,391.545,510.909,373.411z"/>
 <g>
---
+++ src/services/restaurants/mock/index.js
@@ -0,0 +1,21 @@
+import antwerp from "./antwerp.json";
+import chicago from "./chicago.json";
+import toronto from "./toronto.json";
+import san_francisco from "./san_francisco.json";
+
+export const mocks = {
+  "51.219448,4.402464": antwerp,
+  "43.653225,-79.383186": toronto,
+  "41.878113,-87.629799": chicago,
+  "37.7749295,-122.4194155": san_francisco
+};
+
+export const mockImages = [
+  "https://64.media.tumblr.com/68ee6c7a39dd90a22bf93a5639ef80bc/51b87043d2018b8e-2a/s640x960/61f3d2d9801d31cdc235cb914117730990f3af21.jpg",
+  "https://images.unsplash.com/photo-1505394033641-40c6ad1178d7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8cGluayUyMGZvb2R8ZW58MHx8MHx8&w=1000&q=80",
+  "https://article.innovadatabase.com/articleimgs/article_images/637460420432455280exberry%20pink2.jpg",
+  "https://i.guim.co.uk/img/media/82fc4fc8f9f179db7a89a7f830ca1e75d756405e/0_531_5450_3272/master/5450.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=b897dbc1fd6a4f96c44a99dc78ce939a",
+  "https://www.12taste.com/wp-content/uploads/2020/06/Colourfood-powders-colours-donut-chocolate-glaze-pink.jpg",
+  "https://cookingchew.com/wp-content/uploads/2020/08/pink-foods-735x490.jpg",
+  "https://resize.img.allw.mn/food/thumbs/k4/e3/dd5qj9di54f389be08db1729644062_400x400.jpg?width=1200&height=1200"
+];
